c6699bafadcd44616c771e465833792c88c61a71
Viswanath.Ct <2ndryid@gmail.com>
2020-04-11 18:54:30 +0530

    Tweaked the test setup.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,27 @@
+// import declair from 'declair/quick';
+
+// NOTE: Tests are skipped due to a lack of knowledge in setting up the environment.
+
 /* Tests */
 describe('the package', () => {
-	test('root', () => {
+	test.skip('the setup returns a Root and the sources', () => {
+		const { Root, sources } = declair({
+			types: {
+				element: {
+					config: {
+						style: typeStyles.element,
+					},
+				},
+			},
+			sources: {
+				simple: {
+					type: 'config',
+					value: messages[i],
+				},
+			},
+		});
 
+		expect(Root).toEqual(Root);
+		expect(sources).toEqual(sources);
 	});
 });
