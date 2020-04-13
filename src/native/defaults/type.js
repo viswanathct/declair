9cee52ad165c821517847521d092525309d87fc1
Viswanath.Ct <2ndryid@gmail.com>
2020-04-13 19:52:47 +0530

    Fixed an anomally caused by react-native create-stylesheet.

--- src/native/defaults/type.js
+++ src/native/defaults/type.js
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { clone as bypassFreezing } from '@laufire/utils/collection';
 
 const type = {
 	props: {
 		style: {
-			parse: ({ prop }) => StyleSheet.create({ style: prop }).style,
+			parse: ({ prop }) =>
+				StyleSheet.create({ style: bypassFreezing(prop) }).style,
 		},
 	},
 	type: 'uiComponent',
