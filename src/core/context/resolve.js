8f69b90b9d83ab799f6c6082be2c750662b55a5a
Viswanath.Ct <2ndryid@gmail.com>
2020-04-17 17:43:24 +0530

    Fixed resolve to work with non-source string props.

--- src/core/context/resolve.js
+++ src/core/context/resolve.js
@@ -1,6 +1,7 @@
 import { has, traverse, walk } from '@laufire/utils/collection';
 import { inferType } from '@laufire/utils/reflection';
 
+/* Helpers */
 const hasSourceProcessors = {
 	string: (sources, prop) => !!sources[prop],
 	object: (sources, prop) => has(walk(prop,
@@ -15,26 +16,34 @@ const hasSourceWorker = (sources, prop) => {
 		: false;
 };
 
-const resolveProcessors = {
-	string: (state, prop) => state[prop],
-	object: (state, prop) =>
-		traverse(prop, (value) => resolve(state, value)), // eslint-disable-line no-use-before-define
-};
+const resolverBuilder = (context, prop) =>
+	(context.sources[prop]
+		? () => context.state[prop]
+		: () => prop);
 
-const resolve = (state, prop) => {
-	const processor = resolveProcessors[inferType(prop)];
+const buildResolutionTree = (context, prop) =>
+	(inferType(prop) !== 'object'
+		? resolverBuilder(context, prop)
+		: traverse(prop, (value) => resolverBuilder(context, value)));
 
-	return processor
-		? processor(state, prop)
-		: prop;
-};
+const resolve = (resolutionTree) =>
+	(inferType(resolutionTree) !== 'object'
+		? resolutionTree()
+		: traverse(resolutionTree, (propResolver) => propResolver()));
 
+const getTreeResolver = (worker, resolutionTree) =>
+	() => worker(resolve(resolutionTree));
+/* Exports */
 const getResolver = (
 	context, prop, worker
 ) => {
 	const hasSource = hasSourceWorker(context.sources, prop);
+	const resolutionTree = hasSource
+		? buildResolutionTree(context, prop)
+		: null;
+
 	const resolver = hasSource
-		? (() => () => worker(resolve(context.state, prop)))()
+		? getTreeResolver(worker, resolutionTree)
 		: () => worker(prop);
 
 	return { hasSource, resolver };
