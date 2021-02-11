1a74fd7edbdc9a07ce71fa39defad98628b72796
Viswanath.Ct <2ndryid@gmail.com>
2021-02-11 05:54:14 +0530

    Restructured.

--- src/native/types/choice.js
+++ src/native/types/choice.js
@@ -1,6 +1,15 @@
-import selector from './shared/selector';
+import React from 'react';
+import { View } from 'react-native';
 
 /* Exports */
-const choice = selector;
+const choice = {
+	render: ({ item, style }) => {
+		const Item = item();
+
+		return <View { ...{ style: style() } }>
+			<Item/>
+		</View>;
+	},
+};
 
 export default choice;
